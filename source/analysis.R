98badaff2e6d4b8f1d2dde3d27b4266bfe8f01fc
jesusfvargas <jfv97@uw.edu>
2022-11-23 18:06:24 -0800

    final

--- source/analysis.R
+++ source/analysis.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # The functions might be useful for A4
-source("../source/a4-helpers.R")
+source("a4-helpers.R")
 
 ## Test queries ----
 #----------------------------------------------------------------------------#
@@ -29,15 +29,22 @@ test_query2 <- function(num=6) {
 # Your functions might go here ... <todo:  update comment>
 #----------------------------------------------------------------------------#
 # This function ... <todo:  update comment>
-get_year_jail_pop <- function() {
+get_year_jail_pop <- function(df) {
   # TODO: Implement this function 
-return()   
+  d1 <- df %>% group_by(year) %>% 
+    summarise(Black = mean(black_prison_pop, na.rm = T),
+              White = mean(white_prison_pop, na.rm = T))
+  return(d1)   
 }
 
 # This function ... <todo:  update comment>
-plot_jail_pop_for_us <- function()  {
+plot_jail_pop_for_us <- function(df)  {
   # TODO: Implement this function 
-  return()   
+  p1 <- get_year_jail_pop(df)  %>% 
+    ggplot(aes(x = year, y = Black)) +
+    geom_line() + 
+    labs(x = "Year", y = "Average Black population")
+  return(p1)   
 } 
 
 ## Section 4  ---- 
@@ -47,12 +54,32 @@ plot_jail_pop_for_us <- function()  {
 # See Canvas
 #----------------------------------------------------------------------------#
 
+get_year_jail_pop_by_state <- function(df) {
+  # TODO: Implement this function 
+  d1 <- df %>% group_by(year, state) %>% 
+    summarise(Black = mean(black_prison_pop, na.rm = T),
+              White = mean(white_prison_pop, na.rm = T))
+  return(d1)   
+}
+
+# This function ... <todo:  update comment>
+plot_jail_pop_for_us_by_state <- function(df)  {
+  # TODO: Implement this function 
+  p1 <- get_year_jail_pop(df) %>% 
+    ggplot(aes(x = year, y = Black)) +
+    geom_line() + facet_wrap(~state, scales = "free_y") +
+    labs(x = "Year", y = "Average Black population")
+  return(p1)   
+} 
+
 ## Section 5  ---- 
 #----------------------------------------------------------------------------#
 # <variable comparison that reveals potential patterns of inequality>
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
 #----------------------------------------------------------------------------#
+
+
 
 ## Section 6  ---- 
 #----------------------------------------------------------------------------#
@@ -62,5 +89,6 @@ plot_jail_pop_for_us <- function()  {
 #----------------------------------------------------------------------------#
 
 ## Load data frame ---- 
+
 
 
